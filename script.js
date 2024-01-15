08d149d58029d02d809394118aceda408e22c498
mimounellouh <mimounellouh2001@gmail.com>
2024-01-15 11:41:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                lat: 35.561722,
-                lng: -5.362444,
+                lat: 35.61611,
+                lng: -5.281190,
             },
         },
     ];
